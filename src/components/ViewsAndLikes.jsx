38e7d29fcd058a2a7b246ebdf01411db405ed416
Stanley SkimZ Kimambo <Stanleykimambo@gmail.com>
2023-11-18 23:43:26 +0300

    Abit more

--- src/components/ViewsAndLikes.jsx
+++ src/components/ViewsAndLikes.jsx
@@ -1,15 +1,34 @@
-import React from 'react'
-import CommentSection from './CommentSection'
+import React, { useState } from "react";
+import CommentSection from "./CommentSection";
 
 function ViewsAndLikes({ video }) {
+  const [upVotes, setUpVotes] = useState(video.upvotes);
+
+  function handleUpVotes() {
+    // setUpVotes(upVotes)
+    console.log("clicked", upVotes);
+  }
+
+  function handleDownVotes() {
+    let n = -1;
+    return n--;
+  }
+
   return (
     <div>
-        <h2>{video.title}</h2>
-        <p>{video.views} Views | Uploaded {video.created}</p>
-        <span><button>{video.upvotes} 👍🏾</button><button>{video.downvotes} 👎🏾</button></span>
-        <CommentSection video={video} />
+      <h2>{video.title}</h2>
+      <p>
+        {video.views} Views | Uploaded {video.created}
+      </p>
+      <span>
+        <button onClick={handleUpVotes}>{upVotes} 👍🏾</button>
+        <button onClick={handleDownVotes}>
+          {video.downvotes + handleDownVotes()} 👎🏾
+        </button>
+      </span>
+      <CommentSection video={video} />
     </div>
-  )
+  );
 }
 
-export default ViewsAndLikes
+export default ViewsAndLikes;
